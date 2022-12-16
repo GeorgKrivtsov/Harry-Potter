2aa28d93b2ae3e08bbbbf7a7651ec145d077033b
Georgii <Krivtsov.georg@ya.ru>
2022-12-16 14:31:07 +0300

    зменил модификаторы доступа и добавил сеттеры

--- HomeWork/src/boxOfSweets/Sweets.java
+++ HomeWork/src/boxOfSweets/Sweets.java
@@ -1,14 +1,30 @@
 package boxOfSweets;
 public abstract class Sweets {
-    String name;
-    int weight;
-    int price;
+    private String name;
+    private int weight;
+    private int price;
+    private int id;
 
-    Sweets (String name, int weight, int price) {
+    Sweets (String name, int weight, int price, int id) {
         this.name = name;
         this.weight = weight;
         this.price = price;
+        this.id = id;
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public int getWeight() {
+        return weight;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
