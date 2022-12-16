d45b2979b0de7703beda00318f0fcaa6640e6e73
Georgii <Krivtsov.georg@ya.ru>
2022-12-16 14:33:34 +0300

    создал интерфейс BoxInterface

---
+++ HomeWork/src/boxOfSweets/BoxInterface.java
@@ -0,0 +1,15 @@
+package boxOfSweets;
+
+public interface BoxInterface {
+
+
+    void removeSweets();
+
+    void getWeight();
+
+    void getPrice();
+
+    void getAllInfo();
+
+    void addSweets(Object o);
+}
