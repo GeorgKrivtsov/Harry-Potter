d0a7c2c5aed2587af195bdd96f95a3df0c45b4fd
Georgii <Krivtsov.georg@ya.ru>
2022-12-20 13:33:34 +0300

    написал получение массива и определение его в мапу

--- HomeWork/src/TextReading/TextReading.java
+++ HomeWork/src/TextReading/TextReading.java
@@ -1,30 +1,69 @@
 package TextReading;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Arrays;
-import java.util.Scanner;
+import org.w3c.dom.Text;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
 
 public class TextReading {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        HashMap<String, Integer> map = new HashMap<String,Integer>();
 
         System.out.println("Введите путь к файлу:");
+        // /Users/georgijkrivcov/Desktop/текст для примера.txt
 
         String path = sc.nextLine();
+        String[] arrayString = getArrays(path);
+        Arrays.sort(arrayString);
 
-        File file = new File(path);
+        int arraysSize = arrayString.length;
+        System.out.println("В тексте " + arraysSize + " слов");
 
-        Scanner scanner = new Scanner(file);
+        for (String s : arrayString) {
+            if (map.containsKey(s)) {
+                int i = map.get(s);
+                map.put(s, i+1);
 
-        String line = scanner.nextLine();
-        String[] words = line.split(" ");
+            } else {
+                map.put(s,1);
+            }
+        }
 
-        System.out.println(Arrays.toString(words));
+        for (String key : map.keySet()) {
+            Integer value = map.get(key);
+            System.out.println(key + " -> " + value);
+        }
 
 
 
 
+        //System.out.println(Arrays.toString(arrayString));
+
+
+
+
+
+
+
+
+    }
+
+
+    public static String[] getArrays (String path) {
+        try {
+            String lines = Files.readString(Path.of(path));
+            String[] array = lines.split("[ ,.\\n]+");
+            return array;
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new String[0];
     }
 }
