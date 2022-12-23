f1c74c06ec77ed7ba44101d4d9be8389904b84c0
Georgii <Krivtsov.georg@ya.ru>
2022-12-23 17:50:11 +0300

    начал работу по сортировке

--- HomeWork/src/Parser/Main.java
+++ HomeWork/src/Parser/Main.java
@@ -1,8 +1,24 @@
 package Parser;
 
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+
+        JsonParser parser = new JsonParser();
+        Companies companies = parser.parse();
+
+        System.out.println(companies);
+
+        companies.getCompanies().stream()
+
+
+
+
+
+
 
     }
 }
